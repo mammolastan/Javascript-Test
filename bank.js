95abde61b18a76f48312c830aceb1a82233195b8
mammolastan <mammolastan@hotmail.com>
2016-01-11 08:37:52 -0500

    Updated test

--- bank.js
+++ bank.js
@@ -1,67 +1,58 @@
-var balance1 = 0;
-var balance2 = 0;
+//Create a class for all accounts 
+var Account = function(accountType) {
+    this.balance = 0;
+    //accountType used to reference HTML field 
+    this.accountType = accountType;
+};
 
-$(function(){
+//Define the methods that will be standard for all accounts 
+Account.prototype = {
+    //Add desired amount of deposit into the account
+    deposit: function() {
+        this.balance += parseInt($('#amount' + this.accountType).val());
+        this.updateDisplay();
+    },
+    //Withdraw desired amount from account, if possible
+    withdraw: function() {
+        var amount = parseInt($('#amount' + this.accountType).val());;
+        if (amount <= this.balance) {
+            this.balance -= amount
+        }
+        this.updateDisplay();
+    },
+    updateDisplay: function() {
+        this.balance <= 0 ? $('#balance' + this.accountType).addClass('zero') : $('#balance' + this.accountType).removeClass('zero');
+        $('#amount' + this.accountType).val('');
+        $('#balance' + this.accountType).text('$' + this.balance);
+    }
+};
 
-  $('#deposit1').click(depositAccount1);
-  $('#deposit2').click(depositAccount2);
-  $('#withdraw1').click(withdrawAccount1);
-  $('#withdraw2').click(withdrawAccount2);
-  updateDisplay();
+//Create 0bject of Account
+var accountChecking = new Account("Checking");
+
+//Retirement account inherits from Account. Only one method needs to be changed - deposit, to add employer contribution
+function Retirement(accountType, employerContribution) {
+    Account.call(this, accountType);
+    this.employerContribution = employerContribution;
+}
+
+Retirement.prototype = Object.create(Account.prototype);
+Retirement.prototype.constructor = Retirement;
+Retirement.prototype.deposit = function() {
+    //Add desired amount of deposit into the account, plus employer contribution
+    this.balance += parseInt($('#amount' + this.accountType).val()) + this.employerContribution;
+    this.updateDisplay();
+};
+
+var accountRetirement = new Retirement("Retirement", 10);
+
+$(function() {
+
+    $('#depositChecking').click(accountChecking.deposit.bind(accountChecking));
+    $('#depositRetirement').click(accountRetirement.deposit.bind(accountRetirement));
+    $('#withdrawChecking').click(accountChecking.withdraw.bind(accountChecking));
+    $('#withdrawRetirement').click(accountRetirement.withdraw.bind(accountRetirement));
+    accountChecking.updateDisplay();
+    accountRetirement.updateDisplay();
+
 });
-
-function depositAccount1()
-{
-  var amount = $('#amount1').val();
-  amount = parseInt(amount);
-  balance1 = amount + balance1;
-  updateDisplay();
-}
-
-function depositAccount2()
-{
-  var amount = $('#amount2').val();
-  amount = parseInt(amount);
-  amount = amount + 10;
-  balance2 = amount + balance2;
-  updateDisplay();
-}
-
-function withdrawAccount1()
-{
-  var amount = $('#amount1').val();
-  amount = parseInt(amount);
-  if(amount <= balance1) {
-    balance1 = balance1 - amount;
-  }
-  updateDisplay();
-}
-
-function withdrawAccount2()
-{
-  var amount = $('#amount2').val();
-  amount = parseInt(amount);
-  if(amount <= balance2) {
-    balance2 = balance2 - amount;
-  }
-  updateDisplay();
-}
-
-function updateDisplay()
-{
-  if(balance1 <= 0)
-    $('#balance1').addClass('zero');
-  else
-    $('#balance1').removeClass('zero');
-
-  if(balance2 <= 0)
-    $('#balance2').addClass('zero');
-  else
-    $('#balance2').removeClass('zero');
-
-  $('#balance1').text('$' + balance1);
-  $('#balance2').text('$' + balance2);
-  $('#amount1').val('');
-  $('#amount2').val('');
-}
-
